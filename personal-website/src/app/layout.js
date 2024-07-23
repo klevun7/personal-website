9eb12394209f978b6b68524bb24023e9ffd75bca
klevun7 <kevincao64@gmail.com>
2024-07-23 16:58:31 -0700

    first commit

---
+++ personal-website/src/app/layout.js
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google";
+import Footer from "@/components/footer/Footer";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Kevin Cao Personal Website",
+  description: "Kevin Cao's personal website",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className=" w-[1536px] m-auto pl-[50px] pr-[50px] min-h-[100vh] flex flex-col justify-between">
+        {children}
+        <Footer />
+        </div>
+        </body>
+    </html>
+  );
+}
---
+++ personal-website/src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="flex h-screen items-center">
+      <h1 className=" text-5xl font-bold">hi im kevin</h1>
+
+      </div>
+
+    </div>
+  );
+}

b576d9e66f84eb8f239ce9d01ef07c432995ec62
klevun7 <kevincao64@gmail.com>
2025-07-28 15:47:18 -0700

    add analytics tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,8 +12,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <html lang="en">
+    <Analytics>
+      <html lang="en" >
         <head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </head>
@@ -25,6 +25,6 @@ export default function RootLayout({ children }) {
           </div>
         </body>
       </html>
-    </>
+    </Analytics>
   );
 }
